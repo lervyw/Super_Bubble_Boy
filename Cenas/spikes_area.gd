0bdfb00a4da64a98bfd763e8985149d072fd80a3
RansosF0rW4rF4m <riquelmenascimento324@gmail.com>
2025-01-26 17:43:40 -0300

    knockback

--- Cenas/spikes_area.gd
+++ Cenas/spikes_area.gd
@@ -1,19 +1,13 @@
 extends Area2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var spika: Sprite2D = $Spika
-@export var animation: AnimationPlayer
-
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#collision_shape_2d.shape.size = spika.get_rect().size
+func _ready() -> void:
+	collision_shape_2d.shape.size = sprite_2d.get_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_body_entered(body):
-<<<<<<< HEAD
-	if body.name == "Personagem2" && body.has_method("die"):
-=======
-	if body.name == "Player" && body.has_method("die"):
->>>>>>> 04249abbcc840472d86577cb4db29bb062f21f96
-		body.die()
+	if body.has_method("take_damage"):
+		body.take_damage(1)
 	
